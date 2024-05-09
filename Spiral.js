40d29caf8c844bc5c21d3b67c05d8539b6326da4
human-31 <129219923+human-31@users.noreply.github.com>
2024-05-08 22:26:14 -0500

    Add files via upload

--- Spiral.js
+++ Spiral.js
@@ -1,17 +1,32 @@
-const Spiral = {
-    PI: Math.PI,
-    TWO_PI: Math.PI * 2,
+class SpiralApp {
+    #PI = Math.PI;
+    #TWO_PI = Math.PI * 2;
 
     /**
      * Canvas object being used. Set this to the canvas needed.
      * @type {HTMLCanvasElement}
      */
-    canvas,
+    canvas;
     /**
      * Context object being used. Set this to the canvas' context.
      * @type {CanvasRenderingContext2D}
      */
-    context,
+    context;
+
+    constructor(canvas, context) {
+        this.canvas = canvas;
+        this.context = context;
+    }
+
+    /**
+     * Sets the canvas and context properties.
+     * @param {HTMLCanvasElement} canvas Canvas element to assign.
+     * @param {CanvasRenderingContext2D} context Context element to assign.
+     */
+    setup(canvas, context) {
+        this.canvas = canvas;
+        this.context = context;
+    }
 
     /**
      * 
@@ -23,11 +38,9 @@ const Spiral = {
      * @param {number} startAngle Angle spiral starts at relative to `mainAngle`.
      * @param {number} endAngle Angle spiral ends at relative to `mainAngle`.
      */
-    monomialHalfCircle: (x, y, coefficient, degree = 1, mainAngle = 0, startAngle = 0, endAngle = Sprial.TWO_PI) => {
-        
-    },
+    monomialHalfCircle(x, y, coefficient, degree = 1, mainAngle = 0, startAngle = 0, endAngle = this.#TWO_PI) {
 
-    monomialRadial: () => {
-
-    },
+    }
 }
+
+const Spiral = new SpiralApp();
